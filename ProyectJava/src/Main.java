37f9a816c7cccd0b987058ae58d87758baea8f5d
Aquiles735 <aquilesm735@gmail.com>
2025-06-24 16:17:34 -0400

    Java2

---
+++ ProyectJava/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String [] args) {
+        String nombre= " casas ";
+        String cosa="perros ";
+        String trabajos="puptres";
+        int numero=40;
+        int numero1=50;
+        int none=numero*numero1;
+
+        System.out.println("Hola Mundo");
+        System.out.println(none);
+        System.out.println(nombre+cosa+trabajos);
+    }
+
+
+}
+/* Mayus+F10 para Run
+* para abrir carpeta Alt+1*/
+

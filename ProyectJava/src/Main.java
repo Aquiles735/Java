a880ad157a3668f0e5b0923021f798d2d3d12bf4
Aquiles <aquilesm735@gmail.com>
2025-06-25 10:30:51 -0400

    bugs

--- ProyectJava/src/Main.java
+++ ProyectJava/src/Main.java
@@ -1,20 +1,20 @@
 
 public class Main {
-    public static void main(String [] args) {
-        String nombre= " casas ";
-        String cosa="perros ";
-        String trabajos="puptres";
-        int numero=40;
-        int numero1=50;
-        int none=numero*numero1;
-        System.out.println("valor"+""+cosa+ ""+"="+""+ none);
+    public static void main(String[] args) {
+        String nombre = " casas ";
+        String cosa = "perros ";
+        String trabajos = "puptres";
+        int numero = 40;
+        int numero1 = 50;
+        int none = numero * numero1;
+        System.out.println("valor" + "" + cosa + "" + "=" + "" + none);
         System.out.println("Hola mm Mundo");
 
-        System.out.println(nombre+cosa+trabajos);
+        System.out.println(nombre + cosa + trabajos);
+
+
     }
-
-
 }
 /* Mayus+F10 para Run
 * para abrir carpeta Alt+1*/
-
+// comentar linea

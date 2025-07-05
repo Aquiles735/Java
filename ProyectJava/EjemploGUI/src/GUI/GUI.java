cb484189d45ab428eb53ca3d1151f82d84ed5722
Aquiles <aquilesm735@gmail.com>
2025-07-05 17:49:20 -0400

    Exitosa prueba de funcionalidad

--- ProyectJava/EjemploGUI/src/GUI/GUI.java
+++ ProyectJava/EjemploGUI/src/GUI/GUI.java
@@ -4,24 +4,36 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI {
 
-    private JPanel prncipal;
+public class GUI extends JFrame {
+
+    private JPanel panelContenido;
     private JButton borrarButton;
     private JTextField txtMensajed1;
 
     public GUI() {
-        txtMensajed1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        setTitle("Aplicación swing");
+        setSize(500, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setContentPane(panelContenido);
 
-            }
-        });
+
         borrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                txtMensajed1.setText("");
             }
         });
+    }
+           public static void main(String[] args) {
+
+               SwingUtilities.invokeLater(() -> {
+                   new GUI().setVisible(true);
+
+               });
+           }
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
